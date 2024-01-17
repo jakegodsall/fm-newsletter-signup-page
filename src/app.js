f7e558f35e14943315f71c007859d3d736ffed01
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-17 13:25:31 +0000

    added transitions to and from thankyou page

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
-const template = document.getElementById("thankyou-template").content;
-const clone = template.cloneNode(true);
+const form = document.getElementById("signup__form");
 
-document.body.appendChild(clone);
+const mainPage = document.getElementById("main-page");
+const thankyouPage = document.getElementById("thankyou-page");
+const dismissButton = document.getElementById("thankyou__dismissBtn");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  mainPage.classList.add("hidden");
+  thankyouPage.classList.add("show");
+});
+
+dismissButton.addEventListener("click", () => {
+  mainPage.classList.remove("hidden");
+  thankyouPage.classList.remove("show");
+});
