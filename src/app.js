04d26aa7b4994b49fa9ea767f70cdc48b5866933
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-22 09:00:51 +0000

    build FormElementValidator class

--- src/app.js
+++ src/app.js
@@ -1,8 +1,53 @@
 const form = document.getElementById("signup-form");
+const emailElement = document.getElementById("email");
 
 const signupPage = document.getElementById("signup-page");
 const thankyouPage = document.getElementById("thankyou-page");
 const dismissButton = document.getElementById("thankyou-dismissBtn");
+
+function validateEmailString(email) {
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return regex.test(email);
+}
+
+class FormElementValidator {
+  constructor(formElement, validator, errorDOM) {
+    this.formElement = formElement;
+    this.validator = validator;
+    this.errorDOM = errorDOM;
+  }
+
+  validateInput() {
+    return this.validator(this.formElement);
+  }
+
+  makeErrorMessageVisible() {
+    this.errorDOM.classList.add("show");
+  }
+
+  makeErrorMessageInvisble() {
+    this.errorDOM.classList.remove("show");
+  }
+
+  validate() {
+    if (!this.validateInput()) {
+      this.makeErrorMessageVisible(this.errorDOM);
+    } else {
+      this.makeErrorMessageInvisble(this.errorDOM);
+    }
+  }
+}
+
+emailElement.addEventListener("change", () => {
+  const errorDOMElement = document.getElementById("signup__validationMessage");
+  const emailValidator = new FormElementValidator(
+    emailElement,
+    validateEmailString,
+    errorDOMElement
+  );
+
+  emailValidator.validate();
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
